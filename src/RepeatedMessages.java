a2d5a373c4380754a861dbeb437fc7c8f0753442
fillerjeremy <33054901+fillerjeremy@users.noreply.github.com>
2017-11-03 02:36:14 -0700

    1000 lines about cooties
    Made the 1000 line code about Milhouse with give or take 15 lines.

---
+++ src/RepeatedMessages.java
@@ -0,0 +1,15 @@
+
+public class RepeatedMessages {
+	public static void main(String[] args) {
+		for (int i =1; i <1000; i++) {
+			milhouse();
+		}
+	
+		}
+
+	public static void milhouse() {
+		System.out.println("Milhouse did not test cootie positive.");
+		
+	}
+
+}
